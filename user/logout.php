0d693f95cea7adb968958dfc0e59e89570c528c4
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-05-21 19:46:14 +0300

    updated file

--- user/logout.php
+++ user/logout.php
@@ -3,7 +3,7 @@
 	
 	include('conn.php');
 	
-	mysqli_query($con,"update userlog set logout=NOW() where userlogid='".$_SESSION['userlog']."'");
+	 
 	
 	session_destroy();
 	header('location:../index.php');
